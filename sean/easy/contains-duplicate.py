d13ef44f665e655db5817412027a258a08ce28c1
Sean Chang <seanarwa@gmail.com>
2020-09-08 00:27:51 -0700

    ex mode

---
+++ sean/easy/contains-duplicate.py
@@ -0,0 +1,10 @@
+# 9-8-2020
+# https://leetcode.com/problems/contains-duplicate/
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                return True
+        return False
