229adec781d78bad8f0f0d31ea931ac502cbc001
Andrew Nguyen <andrewtnguyen49@gmail.com>
2020-09-09 00:07:31 -0700

    Wednesday - 2 daily leetcode (day 10)

---
+++ andrew/misc/Min Stack.py
@@ -0,0 +1,81 @@
+"""
+
+https://leetcode.com/problems/min-stack/
+
+155. Min Stack (Easy)
+
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+getMin() -- Retrieve the minimum element in the stack.
+ 
+
+Example 1:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+ 
+
+Constraints:
+
+Methods pop, top and getMin operations will always be called on non-empty stacks.
+
+"""
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.min = None
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        if self.min == None:
+            self.min = x
+        elif x < self.min:
+            self.min = x
+        self.stack.append(x)
+
+    def pop(self) -> None:
+        num = self.stack.pop()
+        if num == self.min:
+            if len(self.stack) == 0:
+                self.min = None
+            else:
+                self.min = min(self.stack)
+
+    def top(self) -> int:
+        if len(self.stack) != 0:
+            return self.stack[-1]
+
+    def getMin(self) -> int:
+        if self.min is None:
+            return None
+        else:
+            return self.min
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
