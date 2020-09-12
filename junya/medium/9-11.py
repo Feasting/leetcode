ee370f4f623b9b765944660648a8a7611653b2ff
junyahonda <38553164+junyahonda@users.noreply.github.com>
2020-09-11 23:23:26 -0700

    https://leetcode.com/problems/combination-sum-iv/

--- junya/medium/9-11.py
+++ junya/medium/9-11.py
@@ -3,13 +3,10 @@ class Solution:
         ret = [1]
         for i in range(1, target + 1):
             tot = 0
-            
             for num in nums:
                 if(i - num >= 0):
-                    tot += ret[i - num]
-                    
+                    tot += ret[i - num]      
             ret.append(tot)
-     
         return ret[target]
         
 # https://leetcode.com/problems/combination-sum-iv/
